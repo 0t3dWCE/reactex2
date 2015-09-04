bf0f3d4172e48af8a0721aa704c276a8ed684149
AYakimov <ayakimov@asdco.ru>
2015-09-04 15:59:15 +0300

    Remove removed code (to d3circle/circle.jsx)

--- my_app/src/comments/commentBox.jsx
+++ my_app/src/comments/commentBox.jsx
@@ -2,7 +2,6 @@ var React = require('react');
 var CommentList = require('./commentList');
 var CommentForm = require('./commentForm');
 var D3jscircle = require('./../d3circle/circle')
-var D3js = require('d3');
 
 //Main parent box for all others component
 module.exports = React.createClass({
@@ -40,37 +39,6 @@ module.exports = React.createClass({
 	    setInterval(this.getDataFromSrv, 5000);
 		D3jscircle.circle();
 		//this.circle();
-	},
-	
-	circle: function() {
-		D3js.select("#circle_container").append("circle")
-								.attr("cx", 250)
-								.attr("cy", 250)
-								.attr("r", 1 * (Math.floor(Math.random() * (100 - 50 + 1)) + 50))
-								.attr("fill", "purple")
-								.on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
-								.on("mouseout", function(){d3.select(this).style("fill", "purple");})
-								.on("mousedown", animateFirstStep);
-						
-
-		function animateFirstStep(){
-			d3.select(this)
-			  .transition()            
-				.delay(0)            
-				.duration(1000)
-				.attr("r", 10)
-				.attr("cx", Math.floor(Math.random() * (450 - 50 + 1)) + 50)
-				.attr("cy", Math.floor(Math.random() * (450 - 50 + 1)) + 50)
-				.each("end", animateSecondStep);
-		};
-
-		function animateSecondStep(){
-			d3.select(this)
-			  .transition()
-				.duration(1000)
-				.attr("r", 100)
-				.each("end", animateFirstStep);
-		};
 	},
 	  
 	render: function() {
